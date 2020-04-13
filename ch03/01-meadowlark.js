cee8712d466560e09aaf0eb9e87889e3cf6e681c
thomaspantioras <t.pantioras@gmail.com>
2020-04-13 09:02:05 +0100

    Update ch3

--- ch03/01-meadowlark.js
+++ ch03/01-meadowlark.js
@@ -4,6 +4,12 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+// app.get is the method by which we’re adding routes (app.METHOD eg get,post, ..)
+// it doesn’t care about the case or trailing slash, and it doesn’t consider the querystring when performing the match. 
+// Express defaults to a status code of 200
+//We are also replacing Node’s res.writeHead with res.set and res.status. Express is also providing us a convenience method, res.type, which sets the Content-Type header. While it’s still possible to use res.writeHead and res.end, it isn’t necessary or recommended.
+//  Express’s router strip off the querystring and the trailing slash and convert to lowercase for us automatically. 
+
 app.get('/', (req, res) => {
   res.type('text/plain')
   res.send('Meadowlark Travel')
@@ -13,6 +19,9 @@ app.get('/about', (req, res) => {
   res.type('text/plain')
   res.send('About Meadowlark Travel')
 })
+
+//app.use is the method by which Express adds middleware. think of this as a catchall handler for anything that didn’t get matched by a route. 
+// so in Express, the order in which routes and middleware are added is significant.
 
 // custom 404 page
 app.use((req, res) => {
