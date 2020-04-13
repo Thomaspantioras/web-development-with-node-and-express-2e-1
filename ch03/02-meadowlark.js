08cc31d74cc87a3f82e82f3302acf2f09f227c5d
thomaspantioras <t.pantioras@gmail.com>
2020-04-13 09:03:51 +0100

    update ch4

--- ch03/02-meadowlark.js
+++ ch03/02-meadowlark.js
@@ -3,7 +3,7 @@ const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
-// configure Handlebars view engine
+// configure Handlebars view engine. (creates a view engine and configures Express to use it by default.)
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
 }))
@@ -12,6 +12,9 @@ app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
+
+// the view engine will return a content type of text/html and a status code of 200 by default. 
+// In the catchall handler, which provides our custom 404 page, and the 500 handler, we have to set the status code explicitly.
 
 app.get('/', (req, res) => res.render('home'))
 
--- ch04/lib/fortune.js
+++ ch04/lib/fortune.js
@@ -1,3 +1,4 @@
+// this will be completely hidden.
 const fortuneCookies = [
   "Conquer your fears or they will conquer you.",
   "Rivers need springs.",
@@ -6,6 +7,7 @@ const fortuneCookies = [
   "Whenever possible, keep it simple.",
 ]
 
+// global variable exports. If you want something to be visible outside of the module, you have to add it to exports.
 exports.getFortune = () => {
   const idx = Math.floor(Math.random()*fortuneCookies.length)
   return fortuneCookies[idx]
